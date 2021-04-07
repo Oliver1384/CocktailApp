6b261966b32675edf68ec769b90d6cc7eb12d82a
Oliver1384 <oliver_artiles@hotmail.com>
2021-04-07 17:37:21 +0100

    función mostrar objeto

--- cocktail/main.js
+++ cocktail/main.js
@@ -6,8 +6,17 @@ document.addEventListener("DOMContentLoaded", init);
 function init() {
   var htmlButton = document.getElementById("buscar");
   htmlButton.addEventListener("click",searchCocktail);
+  mostrarObjeto();
 }
 
+
+function mostrarObjeto(){
+  fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a')
+  .then(response => response.json())//convierte objeto json a objeto javascript
+  .then(data => {//añade la imagen al documento
+      console.log(data.drinks[0]);
+  });
+}
 
 
 /**
@@ -28,6 +37,7 @@ function init() {
 function searchCocktail(event){
   event.preventDefault();
   var actual = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='+actualCocktail();
+
   fetch(actual)
   .then(response => response.json())//convierte objeto json a objeto javascript
   .then(data => {//añade la imagen al documento
