cbdd083bec388d2ea58f0c5a706fe9baaa4d2365
Oliver1384 <oliver_artiles@hotmail.com>
2021-04-07 17:13:40 +0100

    version 2

--- cocktail/main.js
+++ cocktail/main.js
@@ -4,15 +4,36 @@ document.addEventListener("DOMContentLoaded", init);
 
 //Busca un cocktail por nombre y devuelve su imagen
 function init() {
- fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
-  .then(response => response.json())
-  .then(data => {
-      var img = document.getElementById("example");
+  var htmlButton = document.getElementById("buscar");
+  htmlButton.addEventListener("click",searchCocktail);
+}
+
+
+
+/**
+ * Optiene el valor de busqueda del html
+ * @return  {String}
+ */
+ function actualCocktail(){
+  let htmlInput = document.getElementById("busquedaCocktail");
+  return htmlInput.value;
+}
+
+
+
+/**
+ * busca y devuelve el objeto 
+ * @return  {Object}
+ */
+function searchCocktail(event){
+  event.preventDefault();
+  var actual = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='+actualCocktail();
+  fetch(actual)
+  .then(response => response.json())//convierte objeto json a objeto javascript
+  .then(data => {//añade la imagen al documento
+      var img = document.getElementById("example2");
       img.src= data.drinks[0].strDrinkThumb;
   });
 }
 
 
-
-
-  
