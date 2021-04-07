8e7ab28cd218a1d49def794b23a30a46e04c4b46
Oliver1384 <56142035+Oliver1384@users.noreply.github.com>
2021-04-07 12:58:09 +0100

    Add files via upload

---
+++ cocktail/main.js
@@ -0,0 +1,26 @@
+// Ubicación de la API --> https://www.thecocktaildb.com/api.php
+
+
+//objeto json de ejemplo convertido a objeto de javascript
+/*var myJSON = '{"name":"John", "age":31, "city":"New York"}';
+var myObj = JSON.parse(myJSON);
+console.log(this.myObj);*/
+
+
+//Busca un cocktail por nombre y devuelve su imagen
+function init() {
+ fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+  .then(response => response.json())
+  .then(data => {
+      console.log(data.drinks[0].strImageSource);
+      // cambiar página
+      var img = document.getElementById("example");
+      img.src= data.drinks[0].strImageSource;
+  });
+}
+
+document.addEventListener("DOMContentLoaded", init);
+
+
+
+  
